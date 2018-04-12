732f6cbb6f7d5d9bee4a86024c50086c87015f70
reddiz <freddiziel@gmail.com>
2018-04-12 11:53:12 +0700

    Add files via upload

---
+++ Cryptopals/Set 1/challenge_5.py
@@ -0,0 +1,19 @@
+inp = """Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
+key = "ICE"
+expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+def xor(ip, ky):
+    cnt = 0
+    ser = []
+    for i in range(len(ip)):
+        tmp = ord(ip[i]) ^ ord(ky[(i % len(key))])
+        ser.append(chr(tmp))
+    res = "".join(ser)
+    return res.encode("hex")
+
+tes = xor(inp, key)
+if tes == expected:
+    print "True!", tes
+else:
+    print "False!", tes
